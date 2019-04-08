339cd449cbbca7dd7f168a15bfd23e6a20c51ebd
Kiran Gulraj <jokiez88@gmail.com>
2019-04-08 15:15:04 +0200

    Introstate with GridBag2

--- view/GameSettingsPanel.java
+++ view/GameSettingsPanel.java
@@ -27,7 +27,6 @@ public class GameSettingsPanel extends JPanel{
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.ipadx = 20;
         add(new JLabel("<html><br><div style='color: white;'>"+"Play as :"+"</div></html>"),gbc);
 
         gbc.gridx = 1;
